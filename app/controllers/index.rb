6b61f4e10e4844f8b04e6e0d06f24099de0e8b4b
Derrick Carr <derrick.the.future@gmail.com>
2017-01-05 18:57:36 -0800

    Add basic auth to token post request.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,8 +1,21 @@
+require 'HTTParty'
+
 get '/' do
   erb :'/index'
 end
 
 get '/clever_login' do
-  
-  redirect to '/clever_details'
+  # Ideally, this is handled asynchronously
+  code = params["code"]
+
+  if code
+    auth = { username: ENV["CLEVER_CLIENT_ID"], password: ENV['CLEVER_CLIENT_SECRET']}
+    @tell_all = "There's a code in this url this time"
+    @url = "https://clever.com/oauth/tokens?code=#{code}&grant_type=authorization_code&redirect_uri=http://localhost:9393/clever_login"
+    @token_request = HTTParty.post(@url, basic_auth: auth)
+  else
+    @tell_all = "NO CODE IN URL"
+  end
+  # redirect to '/clever_details'
+  erb :'/users/profile'
 end
