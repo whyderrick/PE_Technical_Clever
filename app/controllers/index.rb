3da12d7fc859575b38f4b4168325258231a680a5
Derrick Carr <derrick.the.future@gmail.com>
2017-01-08 14:30:17 -0800

    Cleanup commit
    Remove unnecessary variables and create a private method for readability

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,4 @@
 require 'HTTParty'
-require 'base64'
 
 get '/' do
   erb :'/index'
@@ -7,21 +6,40 @@ end
 
 get '/clever_login' do
   # Ideally, this is handled asynchronously
-  # @code = params["code"]
-  # auth = { username: ENV["CLEVER_CLIENT_ID"], password: ENV['CLEVER_CLIENT_SECRET']}
-  @tell_all = "There's a code in this url this time"
-  # @url = "https://clever.com/oauth/tokens?grant_type=authorization_code&redirect_uri=http://localhost:9393/clever_login&code=#{@code}"
-  @url = "https://clever.com/oauth/tokens"
+
   options = {
     body: {
       code: params["code"],
       grant_type: "authorization_code",
-      redirect_uri: "http://localhost:9393/clever_login"
+      redirect_uri: request.url
     },
     basic_auth: {username: ENV["CLEVER_CLIENT_ID"], password: ENV['CLEVER_CLIENT_SECRET']}
   }
-  @token_request = HTTParty.post(@url, options)
+
+  token_request = HTTParty.post("https://clever.com/oauth/tokens", options)
+  p token_request
+  p "\n \n token_request is #{token_request}"
+  token = token_request["access_token"]
+  p "\n \n token is #{token}"
+  session[:token] == token if token
+  p "\n \n session is"
+  session.each{ |k,v| puts "#{k} and #{v}"}
 
   # redirect to '/clever_details'
+  @user_details = get_user_details
   erb :'/users/profile'
 end
+
+get '/profile' do
+  HTTParty.get()
+end
+
+private
+  def get_user_details
+    options = {
+      headers: {
+
+      }
+    }
+    HTTParty.get("https://api.clever.com/me", headers: "Bearer " + session[:token])
+  end
