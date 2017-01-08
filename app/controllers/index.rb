92b96fde7116250326d9ffd835dc9db4e1991f5d
Derrick Carr <derrick.the.future@gmail.com>
2017-01-08 11:29:11 -0800

    Added post body to request and retrieved token.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,5 @@
 require 'HTTParty'
+require 'base64'
 
 get '/' do
   erb :'/index'
@@ -6,16 +7,21 @@ end
 
 get '/clever_login' do
   # Ideally, this is handled asynchronously
-  code = params["code"]
+  # @code = params["code"]
+  # auth = { username: ENV["CLEVER_CLIENT_ID"], password: ENV['CLEVER_CLIENT_SECRET']}
+  @tell_all = "There's a code in this url this time"
+  # @url = "https://clever.com/oauth/tokens?grant_type=authorization_code&redirect_uri=http://localhost:9393/clever_login&code=#{@code}"
+  @url = "https://clever.com/oauth/tokens"
+  options = {
+    body: {
+      code: params["code"],
+      grant_type: "authorization_code",
+      redirect_uri: "http://localhost:9393/clever_login"
+    },
+    basic_auth: {username: ENV["CLEVER_CLIENT_ID"], password: ENV['CLEVER_CLIENT_SECRET']}
+  }
+  @token_request = HTTParty.post(@url, options)
 
-  if code
-    auth = { username: ENV["CLEVER_CLIENT_ID"], password: ENV['CLEVER_CLIENT_SECRET']}
-    @tell_all = "There's a code in this url this time"
-    @url = "https://clever.com/oauth/tokens?code=#{code}&grant_type=authorization_code&redirect_uri=http://localhost:9393/clever_login"
-    @token_request = HTTParty.post(@url, basic_auth: auth)
-  else
-    @tell_all = "NO CODE IN URL"
-  end
   # redirect to '/clever_details'
   erb :'/users/profile'
 end
