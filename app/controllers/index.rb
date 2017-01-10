32b36fd0e52eee064dcc427cc798c165424772c1
Derrick Carr <derrick.the.future@gmail.com>
2017-01-10 15:46:45 -0800

    Add Blog Post and Assets
    This commit adds a blog post explaining how to build this app.
    It also includes minor changes that allow that blog to be written in
    Markdown and to look passable.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,23 +9,24 @@ get '/clever_login' do
     @user_details = get_user_details
     erb :'/_user_details.html', layout: false
   else
-    post_options = {
-      body: {
-        code: params["code"],
-        grant_type: "authorization_code",
-        redirect_uri: request.url.split("?")[0]
-  #  The redirect_uri is also known as a callback url in other contexts. It must
-  # exactly match what's in your application dashboard, including the protocol.
-  # We're grabbing the full url and omitting query parameters.
-      },
-      basic_auth: { username: ENV["CLEVER_CLIENT_ID"],
-                    password: ENV['CLEVER_CLIENT_SECRET'] }
-    }
+    if  params[:code]
+      post_options = {
+        body: {
+          code: params["code"],
+          grant_type: "authorization_code",
+          redirect_uri: request.url.split("?")[0]
+    # We're grabbing the full request url and dumping query parameters.
+        },
+        basic_auth: { username: ENV["CLEVER_CLIENT_ID"],
+                      password: ENV['CLEVER_CLIENT_SECRET'] }
+      }
 
-    token_request = HTTParty.post("https://clever.com/oauth/tokens", post_options)
-    token = token_request["access_token"]
+      token_request = HTTParty.post("https://clever.com/oauth/tokens", post_options)
+      @token = token_request["access_token"]
 
-    session[:token] = token if token
+      session[:token] = @token if @token
+    end
+    
     erb :'/users/profile'
   end
 
@@ -38,7 +39,7 @@ end
 
 
 get '/blog/getting_started' do
-  markdown :'/blog/getting_started'
+  markdown :'/blog/getting_started', layout_engine: :erb
 end
 
 private
